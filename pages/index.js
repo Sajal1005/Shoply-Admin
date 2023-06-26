1aa171b28d040bd3b80545c9521c0f249d61ac20
Sajal1005 <nandasajal.208@gmail.com>
2023-06-26 11:11:55 +0530

    added next-auth

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,35 @@
+import Head from "next/head"
+import { useSession, signIn, signOut } from "next-auth/react"
+
 
 export default function Home() {
+
+  const { data: session } = useSession()
+
+  if(!session){
   return (
-    <div >
-      <h1 className="text-3xl text-center">Hello Sajal</h1>
+    <>
+
+      {/* TITLE */}
+      <Head>
+        <title>Shoply - Streamlined Purchase</title>
+      </Head>
+
+
+
+      <div className="bg-blue-900 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <button className="bg-white p-2 px-4 rounded-lg" onClick={() => signIn('google')}>Login with Google</button>
+        </div>
+      </div>
+      
+    </>
+  )
+  }
+
+  return(
+    <div>
+      Logged In {session.user.email}
     </div>
   )
 }
